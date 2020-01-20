46ecdae61733a3f5c9d4aa473b7a1204bece0c54
Kamko Amoh <b.ottwer@gmail.com>
2020-01-20 17:45:22 +0200

    cipherIndex

--- cipher.ts
+++ cipher.ts
@@ -1,9 +1,3 @@
-import { DataStorageDecorator } from "./Storage/DataStorageDecorator";
-import { AESCipher } from './Cryptography/AESCipher';
-import { DataStorage } from './Storage/DataStorage';
-import { MemoryStorage } from './Storage/MemoryStorage';
-import { FileStorage } from './Storage/FileStorage';
-
 import { CipherController } from './Controller/CipherController';
 
 const inputArg: string[] = process.argv.slice(2);
